c5deba371009b50bfe4559108a8d86c1fc9c54f8
MengtaoZhang <zmt98tony@gmail.com>
2020-06-04 20:23:46 -0500

    finish 56 question with hash table approach.

--- leetcodePractice/56twoSum.py
+++ leetcodePractice/56twoSum.py
@@ -8,9 +8,24 @@ def twoSum(numbers, target):
     @param target: target = numbers[index1] + numbers[index2]
     @return: [index1, index2](index1 < index2)
     
+    # FOR THE QUESTION NOT NEEDING INDEX
     hashset = set()
     for number in numbers:
         if target - number in hashset:
             return number, target - number
         hashset.add(number)
     return [-1, -1]
+
+# FOR THE QUESTION NEEDING INDEX
+def twoSum(numbers, target):
+    
+    if numbers is None:
+        return False
+    
+    target_dict = {}
+    
+    for i in range(len(numbers)):
+        if target - numbers[i] in target_dict:
+            return [target_dict[target - numbers[i]], i]
+        taret_dict[numbers[i]] = i
+    return False
