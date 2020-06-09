ddb6821a1bdcb803b50ef43cb6681d4cfba67389
MengtaoZhang <zmt98tony@gmail.com>
2020-06-09 10:11:51 -0500

    Add a good coding style

---
+++ leetcodePractice/aGoodCodingStyle.py
@@ -0,0 +1,15 @@
+for ....:
+	if !someCondition:
+		continue
+	doSomething
+	doSomething
+	doSomething
+
+Instead of:
+
+for .....:
+	if someCondition:
+		doSomething
+		doSoemthing
+
+Reason:这样可以让缩进更少
